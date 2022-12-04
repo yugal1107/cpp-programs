d29c433f16dbc3b60321425d7e1aa0f165394770
yugal1107 <119532503+yugal1107@users.noreply.github.com>
2022-12-05 00:42:48 +0530

    Delete hello.cpp

--- hello.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-    cout<<"Hello, This is me Yugal. \nNamastey";
-    cout<<"I had made some changes in master branch of the git repository.";
-    
-    return 0;
-}
