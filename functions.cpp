3a55889ca2ca877be527a086a5044f34a213423c
yugal1107 <yugal1107@gmail.com>
2022-12-07 00:07:58 +0530

    Started functions and learned void functions.

---
+++ functions.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+//declaring function without having parameter and function should be defined outside main function.
+void raju() {                     
+        cout << "Hello I am Raju Rastogi...";
+    }
+
+int main(){
+    //Write your cpp code here
+    
+    raju(); //calling of the function 
+    raju();
+    return 0;
+}
